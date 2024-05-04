73b3db8f73a519a0b7336c164f55e607f5902592
Bounpakhong <p.bounpakhong@gmail.com>
2024-05-04 21:57:08 +0700

    update

---
+++ server.js
@@ -0,0 +1,7 @@
+let http = require('http'); //Default module in nodejs
+let dt = require('./myownmodule'); // My own module
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Date: ' + dt.myDateTime());
+    res.end('Hello World');
+}).listen(8000);
